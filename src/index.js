3e7cc8a31d108662efc2e5d0b1f662245af203c2
Jose <jose.lizardo21.cont@gmail.com>
2022-11-16 09:11:33 -0500

    se agregó nuevos archivos

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 
+//initialitation
 const app = express();
 
+
+//setting 
 app.set("port", 3000 || process.env.PORT);
 
+//midlewars
+app.use(express.urlencoded({extended: false}));
+app.use(express.json)
 
-app.listen(app.get("port"), ()=>{
-    console.log(`Server on port ${app.get("port")}`);
+//Routes
+app.use(require('./routes'));
+
+app.listen(app.get('port'), ()=>{
+    console.log(`Server on port ${app.get('port')}`);
 });
