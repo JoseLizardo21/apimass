bb034338c919da9b5c4169cf64110816f89caae9
David53262 <davidcastroastudillo10@gmail.com>
2022-11-21 12:28:54 -0500

    cambios

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
 
-//initialitation
 const app = express();
 
+app.set("port", 3000 || process.env.PORT);
 
-//setting 
-app.set("port", process.env.PORT || 3000);
 
-//midlewars
-app.use(express.urlencoded({extended: false}));
-app.use(express.json())
-
-//Routes
-app.use('/api', require('./routes'));
-
-app.listen(app.get('port'), ()=>{
-    console.log(`Server on port ${app.get('port')}`);
+app.listen(app.get("port"), ()=>{
+    console.log(`Server on port ${app.get("port")}`);
 });
