db7e7252ad2db962f0552f83584669ac3f012dbc
Jose <jose.lizardo21.cont@gmail.com>
2022-11-21 21:21:13 -0500

    se agregó nuevas rutas

--- src/routes/signIn/createAccount.js
+++ src/routes/signIn/createAccount.js
@@ -1,6 +1,17 @@
 const {Router} = require('express');
+const pool = require('../../database');
 const router = Router();
-router.get('/', (req, res)=>{
+router.post('/', async (req, res)=>{
+    const {usuario_nombre, usuario_apellido, usuario_numero, usuario_email, usuario_password} = req.body;
+    const user = {
+        usuario_nombre,
+        usuario_apellido,
+        usuario_numero,
+        usuario_email,
+        usuario_password
+    }
+    await pool.query('INSERT INTO usuario set ?', [user]);
+    res.send("Se ha enviado correctamente");
 });
 
 module.exports = router;
