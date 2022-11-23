e9347f9dd97347e6ca328232fb2df5f30bd51058
David47742412 <davidcastroastudillo20@gmail.com>
2022-11-23 12:12:16 -0500

    cambios

--- src/routes/signIn/createAccount.js
+++ src/routes/signIn/createAccount.js
@@ -1,20 +1,48 @@
 const {Router} = require('express');
+const { check } = require('express-validator');
 const pool = require('../../database');
 const router = Router();
 const helpers = require('../../lib/helpers');
 
 router.post('/', async (req, res)=>{
-    const {usuario_nombre, usuario_apellido, usuario_numero, usuario_email, usuario_password} = req.body;
-    const user = {
-        usuario_nombre,
-        usuario_apellido,
-        usuario_numero,
-        usuario_email,
-        usuario_password
+    try {
+        const { usuario_nombre, usuario_apellido, usuario_numero, usuario_email, usuario_password, usuario_token, usuario_create, usuario_update } = req.body;
+        const user = {
+            usuario_nombre,
+            usuario_apellido,
+            usuario_numero,
+            usuario_email,
+            usuario_password,
+            usuario_token,
+            usuario_create,
+            usuario_update,
+        }
+
+        /*
+        check('usuario_dni')
+        .length(8)
+        .isString()
+        .isNumeric()
+        */
+
+        user.usuario_create = await helpers.getDateNow();
+        
+        user.usuario_update = await helpers.getDateNow();
+
+        user.usuario_password = await helpers.encrypPassword(usuario_password);
+    
+        user.usuario_token = undefined;
+    
+        user.usuario_token = await helpers.getToken(user);
+
+    
+        await pool.query('INSERT INTO usuario set ?', [user]);
+    
+        res.send("user");
+        
+    } catch (error) {
+        res.send(error);
     }
-    user.usuario_password = await helpers.encrypPassword(usuario_password);
-    await pool.query('INSERT INTO usuario set ?', [user]);
-    res.send("Se ha enviado correctamente");
 });
 
 module.exports = router;
