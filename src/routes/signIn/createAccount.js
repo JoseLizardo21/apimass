12d1430d0711d5557138b3d0e31c818f79ba439d
David47742412 <davidcastroastudillo20@gmail.com>
2022-11-23 08:14:36 -0500

    cambios

--- src/routes/signIn/createAccount.js
+++ src/routes/signIn/createAccount.js
@@ -1,6 +1,8 @@
 const {Router} = require('express');
 const pool = require('../../database');
 const router = Router();
+const hash = require('bcrypt')
+
 router.post('/', async (req, res)=>{
     const {usuario_nombre, usuario_apellido, usuario_numero, usuario_email, usuario_password} = req.body;
     const user = {
@@ -10,6 +12,9 @@ router.post('/', async (req, res)=>{
         usuario_email,
         usuario_password
     }
+
+    user.usuario_password = hash(user.usuario_password);
+
     await pool.query('INSERT INTO usuario set ?', [user]);
     res.send("Se ha enviado correctamente");
 });
